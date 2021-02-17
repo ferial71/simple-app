4159d6a43c95b3683d165ad03d27f8fc39292460
ferial71 <ff_bouzoul@esi.dz>
2021-02-17 17:40:43 +0100

    added home component and simple function

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  clickCounter: number = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  countClick(){
+    this.clickCounter += 1
+  }
+
+}
